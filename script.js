80342b23adb89d12ebf0a6ff0734609243d38c72
Harsh <techharsh05@gmail.com>
2024-12-24 00:32:22 +0530

    Email API Attached

--- script.js
+++ script.js
@@ -152,3 +152,26 @@ sound.addEventListener('click',()=>{
         bgaudio.play();
     }
 })
+
+
+//Email API
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_gqlzp43';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
